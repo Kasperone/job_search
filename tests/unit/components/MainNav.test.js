378714f23e6dc6ced097bdef165e89c630e22382
Kasperone <kosecki.kasper@gmail.com>
2022-07-14 22:22:16 +0200

    testing_the_v-for_done

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,20 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Super Careers");
   });
+
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Super Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
