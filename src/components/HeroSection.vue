45b34ad6f5ea9ac1e791a8af5dd5e9ef82ac6eab
Kasperone <kosecki.kasper@gmail.com>
2022-07-18 23:42:41 +0200

    added_hero_headline_component_and_working_on_it

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,11 +1,31 @@
 <template>
   <main>
-    <section>I cab be your Hero</section>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-slate-400">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-700">
+          1 column
+        </div>
+        <div class="col-start-2 col-span-5 border border-blue-700">
+          <hero-headline />
+        </div>
+        <div class="col-start-7 col-span-5 border border-blue-700">
+          5 columns
+        </div>
+        <div class="col-start-12 col-span-1 border border-blue-700">
+          1 columns
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
+import HeroHeadline from "@/components/HeroHeadline.vue";
+
 export default {
   name: "HeroSection",
+  components: {
+    HeroHeadline,
+  },
 };
 </script>
