f1a3ff67108d6a398989b5755dcbfb998b9f7b94
Kasperone <kosecki.kasper@gmail.com>
2022-07-18 13:56:30 +0200

    added_search_icon_and_implemented_v-if_first_functionality

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -2,7 +2,12 @@
   <div
     class="flex w-full items-center h-16 bg-white border-b border-solid border-brand-gray-1"
   >
-    <div class="f-full px-8">This is SubNav</div>
+    <div class="f-full px-8">
+      <div>
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span><span class="text-brand-green-1">1666</span> jobs matched</span>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
 
