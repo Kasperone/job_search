bd6019af0e92b4da5e221c5e824b57a2b2e38d06
Kasperone <kosecki.kasper@gmail.com>
2022-07-19 23:53:45 +0200

    created_job_search_form_component_added_styles

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,19 +1,14 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-slate-400">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-700">
-          1 column
-        </div>
-        <div class="col-start-2 col-span-5 border border-blue-700">
+        <div class="col-start-1 col-span-1">1 column</div>
+        <div class="col-start-2 col-span-5">
           <hero-headline />
+          <job-search-form />
         </div>
-        <div class="col-start-7 col-span-5 border border-blue-700">
-          5 columns
-        </div>
-        <div class="col-start-12 col-span-1 border border-blue-700">
-          1 columns
-        </div>
+        <div class="col-start-7 col-span-5">5 columns</div>
+        <div class="col-start-12 col-span-1">1 columns</div>
       </div>
     </section>
   </main>
@@ -21,11 +16,13 @@
 
 <script>
 import HeroHeadline from "@/components/HeroHeadline.vue";
+import JobSearchForm from "./JobSearchForm.vue";
 
 export default {
   name: "HeroSection",
   components: {
     HeroHeadline,
+    JobSearchForm,
   },
 };
 </script>
