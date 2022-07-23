a9b7ec2fc6df5eee99811767d968b0dc3b99c63e
Kasperone <kosecki.kasper@gmail.com>
2022-07-23 20:18:58 +0200

    configured_store_index_file

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,25 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+// export default createStore({
+//   state: {},
+//   getters: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {},
+// });
+
+const store = createStore({
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    LOGIN_USER(state) {
+      state.isLoggedIn = true;
+    },
+  },
+  strict: process.env.NODE_ENV !== "production",
 });
+
+export default store;
