9b340c57179263c8bf17b6c21dfd604221504abf
Kasperone <kosecki.kasper@gmail.com>
2022-07-23 22:41:54 +0200

    created_constant_for_mutation

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ import { createStore } from "vuex";
 //   modules: {},
 // });
 
+export const LOGIN_USER = "LOGIN_USER";
+
 export const state = () => {
   return {
     isLoggedin: false,
@@ -15,7 +17,7 @@ export const state = () => {
 };
 
 export const mutations = {
-  LOGIN_USER(state) {
+  [LOGIN_USER](state) {
     state.isLoggedIn = true;
   },
 };
