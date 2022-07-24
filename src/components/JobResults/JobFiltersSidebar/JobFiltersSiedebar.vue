12ab58866db2af9784bf8243f90dcbf2c2c235fe
Kasperone <kosecki.kasper@gmail.com>
2022-07-24 23:07:10 +0200

    starts_working_on_jobs_filters_continue_in_next_branch

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSiedebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSiedebar.vue
@@ -2,11 +2,18 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    Job filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm"></div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+
 export default {
   name: "JobFiltersSidebar",
 };
