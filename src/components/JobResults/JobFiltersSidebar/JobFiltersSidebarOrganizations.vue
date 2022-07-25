254dd693e788a0497d146b8c4aca42886c0d144f
Kasperone <kosecki.kasper@gmail.com>
2022-07-25 09:08:16 +0200

    created_job_filters_sidebar_organizations_and_accordion_filters_components

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,35 @@
+<template>
+  <accordion-filters header="Organizations" class="mt-5">
+    <fieldset>
+      <ul class="flex flex-row flex-wrap">
+        <li class="w-1/2 h-8">
+          <input id="VueTube" type="checkbox" class="mr-3" />
+          <label for="VueTube">VueTube</label>
+        </li>
+        <li class="w-1/2 h-8">
+          <input id="Between Vue Me" type="checkbox" class="mr-3" />
+          <label for="Between Vue Me">Between Vue</label>
+        </li>
+        <li class="w-1/2 h-8">
+          <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+          <label for="Et Vue Brute">Et Vue Brute</label>
+        </li>
+        <li class="w-1/2 h-8">
+          <input id="Vue and You" type="checkbox" class="mr-3" />
+          <label for="Vue and You">Vue and You</label>
+        </li>
+      </ul>
+    </fieldset>
+  </accordion-filters>
+</template>
+
+<script>
+import AccordionFilters from "@/components/Shared/AccordionFilters.vue";
+
+export default {
+  name: "JobFiltersSidebarOrganizations",
+  components: {
+    AccordionFilters,
+  },
+};
+</script>
--- src/components/JobResults/JobFiltersSidebar/JobFiltersSiedebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSiedebar.vue
@@ -6,20 +6,31 @@
       <div class="flex flex-row justify-between">
         <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
         <div class="flex items-center text-sm">
-          <action-button />
+          <action-button text="Clear Filters" type="secondary" />
         </div>
       </div>
+
+      <accordion-filters header="Dagree" />
+
+      <accordion-filters header="Job types">
+        <p>Placeholder for now</p>
+      </accordion-filters>
+      <job-filters-sidebar-organizations />
     </section>
   </div>
 </template>
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import AccordionFilters from "@/components/Shared/AccordionFilters.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 
 export default {
   name: "JobFiltersSidebar",
   components: {
     ActionButton,
+    AccordionFilters,
+    JobFiltersSidebarOrganizations,
   },
 };
 </script>
